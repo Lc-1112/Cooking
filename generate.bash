ccca7999d2ceef809a50daf3b1c710b68d053a19
Lc-1112 <921086377@qq.com>
2025-09-28 13:32:33 +0800

    糖醋排骨，水煮肉片

--- generate.bash
+++ generate.bash
@@ -12,9 +12,9 @@ pandoc "$INPUT_FILE" -o "$OUTPUT_FILE" \
 --pdf-engine=xelatex \
 --toc \
 --toc-depth=3 \
--V toc-title="目录" \
+-V toc-title="自用已做过菜谱(不定期更新)" \
 -V CJKmainfont="仿宋" \
--V geometry:"left=1cm,right=1cm,top=1cm,bottom=1.5cm"
+-V geometry:"left=1cm,right=1cm,top=1.5cm,bottom=1.5cm"
 
 # 检查是否成功生成
 if [ -f "$OUTPUT_FILE" ]; then
